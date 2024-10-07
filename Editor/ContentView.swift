0af3cac562a3550f86acdcfd7287a746f6639044
noViceMin <admin@isamin.kr>
2024-10-07 14:18:05 +0900

    Initial Commit

---
+++ Editor/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ██   ██ ██████  ██ ███████  █████  ███    ███ ██ ███    ██
+//  ██  ██  ██   ██ ██ ██      ██   ██ ████  ████ ██ ████   ██
+//  █████   ██████  ██ ███████ ███████ ██ ████ ██ ██ ██ ██  ██
+//  ██  ██  ██   ██ ██      ██ ██   ██ ██  ██  ██ ██ ██  ██ ██
+//  ██   ██ ██   ██ ██ ███████ ██   ██ ██      ██ ██ ██   ████
+//
+//  https://isamin.kr
+//  https://github.com/krisamin
+//
+//  Created : 10/7/24
+//  Package : Editor
+//  File    : ContentView.swift
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Editor/EditorApp.swift
@@ -0,0 +1,25 @@
+//
+//  ██   ██ ██████  ██ ███████  █████  ███    ███ ██ ███    ██
+//  ██  ██  ██   ██ ██ ██      ██   ██ ████  ████ ██ ████   ██
+//  █████   ██████  ██ ███████ ███████ ██ ████ ██ ██ ██ ██  ██
+//  ██  ██  ██   ██ ██      ██ ██   ██ ██  ██  ██ ██ ██  ██ ██
+//  ██   ██ ██   ██ ██ ███████ ██   ██ ██      ██ ██ ██   ████
+//
+//  https://isamin.kr
+//  https://github.com/krisamin
+//
+//  Created : 10/7/24
+//  Package : Editor
+//  File    : EditorApp.swift
+//
+
+import SwiftUI
+
+@main
+struct EditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
